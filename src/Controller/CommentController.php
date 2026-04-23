b0872bb67662565fa01175ddd6a3cf75f82a3f38
dmytro tverdokhlib <ditverd@gmail.com>
2026-04-23 19:07:31 +0300

    index Comments

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CommentController extends AbstractController
 {
     #[Route('/comment', name: 'app_comment')]
-    public function index(): Response
+    public function index(CommentRepository $commentRepository): Response
     {
-        dd('hello');
-
         return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+            'comments' => $commentRepository->findAll(),
         ]);
     }
 }
