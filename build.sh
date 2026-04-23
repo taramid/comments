9a2dc3094341a6b8e97845769c96c94a1d17a94b
dmytro tverdokhlib <ditverd@gmail.com>
2026-04-23 23:30:55 +0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+##!/bin/bash
+## This script handles the multi-arch build and exports it to a file
+#echo "Building image for linux/amd64..."
+#docker buildx build \
+#  --platform linux/amd64 \
+#  --pull \
+#  --no-cache \
+#  -f compose.yaml \
+#  -f compose.prod.yaml \
+#  -t app-php-prod:latest \
+#  --output type=docker,dest=app.tar .
+#echo "Build complete: app.tar created."
+#
+# з цим підохдом потрібно якось окремо тягти змінні з *.yaml файлів, що трохи морочливо
+#
+# тому поки будемо створювати образ на linux-сервері
+
+docker compose -f compose.yaml -f compose.prod.yaml build --pull --no-cache
